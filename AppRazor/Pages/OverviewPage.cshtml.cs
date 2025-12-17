cec863b3a29bd63fc858ab0e755ab690fd27d3be
Amanda Kilström <Amanda.kilstrom@hotmail.com>
2025-12-17 14:52:53 +0100

    overview done

--- AppRazor/Pages/OverviewPage.cshtml.cs
+++ AppRazor/Pages/OverviewPage.cshtml.cs
@@ -1,71 +1,56 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Models.Interfaces;
-using Services.Interfaces;
-using System.Linq;
-using Models;
+using DbContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppRazor.Pages
 {
     public class OverviewPageModel : PageModel
     {
-        private readonly IFriendsService _friendsService;
-        private readonly IAddressesService _addressService;
+        private readonly MainDbContext _context;
         private readonly ILogger<OverviewPageModel> _logger;
 
         [BindProperty]
-        public List<IAddress> Addresses { get; set; } = new();
+        public List<CountryCityOverview> Overview { get; set; }
 
-        [BindProperty]
-        public List<Country> Countries { get; set; }
-
-        public OverviewPageModel(IFriendsService friendsService, IAddressesService addressesService, ILogger<OverviewPageModel> logger)
+        public OverviewPageModel(MainDbContext context, ILogger<OverviewPageModel> logger)
         {
-            _friendsService = friendsService;
-            _addressService = addressesService;
+            _context = context;
             _logger = logger;
         }
 
         public async Task<IActionResult> OnGet()
         {
-            var friends = await _friendsService.ReadFriendsAsync(true, false, null, 0, 10);
+            var friends = await _context.InfoFriendsView.ToListAsync();
+            var pets = await _context.InfoPetsView.ToListAsync();
 
-            var addresses = await _addressService.ReadAddressesAsync(true, false, null, 0, 10);
-
-            Countries = Country.GetCountries(addresses.PageItems);
+            Overview = friends
+                .GroupJoin(
+                    pets,
+                    f => new { f.Country, f.City },
+                    p => new { p.Country, p.City },
+                    (f, p) => new CountryCityOverview
+                    {
+                        Country = f.Country,
+                        City = f.City,
+                        FriendCount = f.NrFriends,
+                        PetsCount = p.FirstOrDefault()?.NrPets ?? 0
+                    })
+                .OrderBy(x => x.Country)
+                .ThenBy(x => x.City)
+                .Where(o => !string.IsNullOrWhiteSpace(o.Country))
+                .ToList();
 
             return Page();
         }
 
-        public class Country
+        public class CountryCityOverview
         {
-            public string Name { get; set; }
-            static int NrOfCountries { get; set; }
+            public string Country { get; set; }
+            public string City { get; set; }
 
-            public Country(string name, int nrOfCountries)
-            {
-                Name = name;
-                NrOfCountries = nrOfCountries;
-            }
-
-            public static List<Country> GetCountries(List<IAddress> addresses)
-            {
-                List<Country> countries = new();
-                int currentCount = 1;
-
-                foreach (var address in addresses.DistinctBy(c => c.Country))
-                {
-                    Country country = new Country(address.Country, currentCount);
-                    countries.Add(country);
-
-                    currentCount++;
-                }
-
-                NrOfCountries = currentCount;
-
-                return countries;
-            }
+            public int FriendCount { get; set; }
+            public int PetsCount { get; set; }
         }
     }
 }
